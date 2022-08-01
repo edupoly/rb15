23dc8657c018d29ea96c540c64e83b4a42c707c9
edupoly <info.edupoly@gmail.com>
2022-08-01 07:58:39 +0530

    axios get countries

--- src/store/reducers/actions.js
+++ src/store/reducers/actions.js
@@ -1,10 +1,10 @@
+import axios from 'axios';
 export function getAllCountries(){
+    console.dir(axios)
     return (dispatch)=>{
-        fetch("https://restcountries.com/v2/all")
-        .then((res)=>res.json())
-        .then((data)=>{
-            dispatch({type:'UPDATE_COUNTRIES',payload:data})
+        axios.get("https://restcountries.com/v2/all")
+        .then(response=>{
+            dispatch({type:'UPDATE_COUNTRIES',payload:response.data})
         })
     }
-    
 }
