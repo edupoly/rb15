c824dd53d2198c14d90d5ff2a261ea1d0c26ec12
edupoly <info.edupoly@gmail.com>
2022-08-07 09:16:22 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './Header'
-import Footer from './Footer';
-import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <div className='betterview'>
-        <Outlet></Outlet>
-      </div>
-      <Footer></Footer>
+      <h1>Welcome to redux toolkit</h1>
     </div>
   );
 }
